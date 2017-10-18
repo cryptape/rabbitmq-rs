35c8ebd3d87350129eca186c2b608fa50c0d0075
zhangsoledad <787953403@qq.com>
2017-10-18 21:09:32 +0800

    static link

--- librabbitmq-sys/build.rs
+++ librabbitmq-sys/build.rs
@@ -1,82 +1,34 @@
-extern crate cmake;
-extern crate cc;
-extern crate pkg_config;
 extern crate bindgen;
+extern crate cc;
+extern crate cmake;
+extern crate pkg_config;
 
-use std::env;
-use std::ffi::OsString;
-use std::fs::{self, File};
-use std::io::prelude::*;
-use std::path::{Path, PathBuf};
+use std::path::Path;
 use std::process::Command;
 
 fn main() {
-    build_librabbitmq();
-
-    let bindings = bindgen::Builder::default()
-        // The input header we would like to generate
-        // bindings for.
-        .header("wrapper.h")
-        // Finish the builder and generate the bindings.
-        .generate()
-        // Unwrap the Result and panic on failure.
-        .expect("Unable to generate bindings");
-
-    // Write the bindings to the $OUT_DIR/bindings.rs file.
-    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
-    bindings
-        .write_to_file(out_path.join("bindings.rs"))
-        .expect("Couldn't write bindings!");
-}
-
-fn build_librabbitmq() {
-    let has_pkgconfig = Command::new("pkg-config").output().is_ok();
-
-    if let Ok(lib) = pkg_config::find_library("librabbitmq") {
-        for path in &lib.include_paths {
-            println!("cargo:include={}", path.display());
-        }
-        return
-    }
-
     if !Path::new("librabbitmq/.git").exists() {
-        let _ = Command::new("git").args(&["submodule", "update", "--init"])
-                                   .status();
+        let _ = Command::new("git")
+            .args(&["submodule", "update", "--init"])
+            .status();
     }
-
     let mut cfg = cmake::Config::new("librabbitmq");
 
+    let dst = cfg.define("BUILD_EXAMPLES", "OFF")
+        .define("BUILD_SHARED_LIBS", "OFF")
+        .define("BUILD_STATIC_LIBS", "ON")
+        .define("BUILD_TESTS", "OFF")
+        .define("BUILD_TOOLS", "ON")
+        .define("BUILD_TOOLS_DOCS", "OFF")
+        .define("ENABLE_SSL_SUPPORT", "ON")
+        .define("ENABLE_THREAD_SAFETY", "ON")
+        .define("BUILD_API_DOCS", "OFF")
+        .register_dep("OPENSSL")
+        .build();
 
-    let dst = cfg.define("BUILD_SHARED_LIBS", "OFF")
-                 .define("BUILD_STATIC_LIBS", "ON")
-                 .define("BUILD_EXAMPLES", "OFF")
-                 .build();
-
-    println!("cargo:rustc-link-lib=static=librabbitmq");
-    println!("cargo:rustc-link-search=native={}/lib", dst.display());
+    println!(
+        "cargo:rustc-link-search=native={}/lib/x86_64-linux-gnu",
+        dst.display()
+    );
+    println!("cargo:rustc-link-lib=static=rabbitmq");
 }
-
-fn register_dep(dep: &str) {
-    if let Some(s) = env::var_os(&format!("DEP_{}_ROOT", dep)) {
-        prepend("PKG_CONFIG_PATH", Path::new(&s).join("lib/pkgconfig"));
-        return
-    }
-    if let Some(s) = env::var_os(&format!("DEP_{}_INCLUDE", dep)) {
-        let root = Path::new(&s).parent().unwrap();
-        env::set_var(&format!("DEP_{}_ROOT", dep), root);
-        let path = root.join("lib/pkgconfig");
-        if path.exists() {
-            prepend("PKG_CONFIG_PATH", path);
-            return
-        }
-    }
-}
-
-fn prepend(var: &str, val: PathBuf) {
-    let prefix = env::var(var).unwrap_or(String::new());
-    let mut v = vec![val];
-    v.extend(env::split_paths(&prefix));
-    env::set_var(var, &env::join_paths(v).unwrap());
-}
-
-
--- librabbitmq-sys/lib.rs
+++ librabbitmq-sys/lib.rs
@@ -1,13 +1,10 @@
-#![allow(non_upper_case_globals)]
-#![allow(non_camel_case_types)]
-#![allow(non_snake_case)]
-
 extern crate libc;
 
-use libc::{c_int, c_char, c_uint, size_t, c_uchar, c_void};
-use std::ptr;
+mod bindings;
 
-include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+use libc::{c_char, c_int, c_uchar, c_uint, c_void, size_t};
+use std::ptr;
+pub use bindings::*;
 
 
 #[cfg(test)]
@@ -20,19 +17,21 @@ mod tests {
         unsafe {
             let conn = amqp_new_connection();
             let socket = amqp_tcp_socket_new(conn);
-            let status = amqp_socket_open(socket, b"localhost\0".as_ptr() as *const c_char , 5672);
+            let status = amqp_socket_open(socket, b"localhost\0".as_ptr() as *const c_char, 5672);
             println!("status {}", status);
-            let login = amqp_login(conn, b"/\0".as_ptr() as *const c_char,
+            let login = amqp_login(
+                conn,
+                b"/\0".as_ptr() as *const c_char,
                 0,
                 131072,
                 0,
                 amqp_sasl_method_enum::AMQP_SASL_METHOD_PLAIN,
                 b"guest\0".as_ptr() as *const c_char,
-                b"guest\0".as_ptr() as *const c_char
+                b"guest\0".as_ptr() as *const c_char,
             );
+
             println!("login {:?}", login);
             let channel = amqp_channel_open(conn, 1);
-
             let reply = amqp_get_rpc_reply(conn);
             println!("reply {:?}", reply);
 
@@ -42,8 +41,8 @@ mod tests {
             //     pub consumer_count: u32,
             // }
 
-            let queue_declare: *mut amqp_queue_declare_ok_t = amqp_queue_declare(conn, 1, amqp_empty_bytes, 0, 0, 0, 1,
-                                 amqp_empty_table);
+            let queue_declare: *mut amqp_queue_declare_ok_t =
+                amqp_queue_declare(conn, 1, amqp_empty_bytes, 0, 0, 0, 1, amqp_empty_table);
 
             println!("queue_declare {:?}", queue_declare);
 
@@ -54,15 +53,28 @@ mod tests {
 
             println!("{:?}", queue_name);
 
-            amqp_queue_bind(conn, 1, queue_name,
+            amqp_queue_bind(
+                conn,
+                1,
+                queue_name,
                 amqp_cstring_bytes(b"amq.direct\0".as_ptr() as *const c_char),
                 amqp_cstring_bytes(b"test\0".as_ptr() as *const c_char),
-                amqp_empty_table);
+                amqp_empty_table,
+            );
 
             let reply = amqp_get_rpc_reply(conn);
             println!("reply {:?}", reply);
 
-            amqp_basic_consume(conn, 1, queue_name, amqp_empty_bytes, 0, 1, 0, amqp_empty_table);
+            amqp_basic_consume(
+                conn,
+                1,
+                queue_name,
+                amqp_empty_bytes,
+                0,
+                1,
+                0,
+                amqp_empty_table,
+            );
             let reply = amqp_get_rpc_reply(conn);
             println!("reply {:?}", reply);
 
@@ -83,19 +95,29 @@ mod tests {
             //     pub pool: amqp_pool_t,
             // }
 
-            let envelope: *mut amqp_envelope_t = libc::malloc(mem::size_of::<amqp_envelope_t>()) as *mut amqp_envelope_t;
+            let envelope: *mut amqp_envelope_t =
+                libc::malloc(mem::size_of::<amqp_envelope_t>()) as *mut amqp_envelope_t;
             amqp_maybe_release_buffers(conn);
 
-            let res = amqp_consume_message(conn, envelope, ptr::null_mut() , 0);
+            let res = amqp_consume_message(conn, envelope, ptr::null_mut(), 0);
             println!("res {:?}", res);
 
             println!("Delivery {:?}", *envelope);
 
             amqp_destroy_envelope(envelope);
 
-            println!("amqp_channel_close {:?}", amqp_channel_close(conn, 1, AMQP_REPLY_SUCCESS as c_int));
-            println!("amqp_connection_close {:?}", amqp_connection_close(conn, AMQP_REPLY_SUCCESS as c_int));
-            println!("amqp_destroy_connection {:?}", amqp_destroy_connection(conn));
+            println!(
+                "amqp_channel_close {:?}",
+                amqp_channel_close(conn, 1, AMQP_REPLY_SUCCESS as c_int)
+            );
+            println!(
+                "amqp_connection_close {:?}",
+                amqp_connection_close(conn, AMQP_REPLY_SUCCESS as c_int)
+            );
+            println!(
+                "amqp_destroy_connection {:?}",
+                amqp_destroy_connection(conn)
+            );
         }
     }
 }
